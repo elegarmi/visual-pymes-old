02a4f35e904397de6bfacb950047307589f8f6f4
elegarmi <elegarmi@gmail.com>
2022-02-17 18:12:25 +0100

    Añadido footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../Home/Footer/Footer.jsx";
 import Header from "../Home/Header/Header.jsx";
+import Footer from "../Home/Footer/Footer.jsx";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
